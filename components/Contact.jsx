1918ce644d3d076f99b8c51a99098ec69e92374f
Rakibul Islam Sumon <learnwithsumon@gmail.com>
2025-02-28 14:22:30 +0600

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,24 +5,19 @@ import microsoft from "@/public/microsoft.png";
 
 export default function Contact() {
   return (
-     <div className="flex justify-center items-center">
-       <h2 className="text-xl text-center font-semibold">
-          Get App from
-        </h2>
-          <div className="flex gap-4 mt-10 mb-20">
-            <a href="https://www.effectiveratecpm.com/ebmi8eah?key=f08d3ac8bfcaba61b928eb1819245b99">
-              <Image src={GooglePlay} alt="" width={30} />
-            </a>
-            <a href="https://www.effectiveratecpm.com/ebmi8eah?key=f08d3ac8bfcaba61b928eb1819245b99">
-              <Image src={appStore} alt="" width={30} />
-            </a>
-            <a
-              href="https://www.effectiveratecpm.com/ebmi8eah?key=f08d3ac8bfcaba61b928eb1819245b99"
-              className=""
-            >
-              <Image src={microsoft} alt="" width={120} />
-            </a>
-          </div>
-        </div>
+    <div>
+      <h2 className="text-xl text-center font-semibold mt-10">Get App from</h2>
+      <div className="flex gap-4 mt-10 mb-20">
+        <a href="https://www.effectiveratecpm.com/ebmi8eah?key=f08d3ac8bfcaba61b928eb1819245b99">
+          <Image src={GooglePlay} alt="" width={30} />
+        </a>
+        <a href="https://www.effectiveratecpm.com/ebmi8eah?key=f08d3ac8bfcaba61b928eb1819245b99">
+          <Image src={appStore} alt="" width={30} />
+        </a>
+        <a href="https://www.effectiveratecpm.com/ebmi8eah?key=f08d3ac8bfcaba61b928eb1819245b99">
+          <Image src={microsoft} alt="" width={130} />
+        </a>
+      </div>
+    </div>
   );
 }
